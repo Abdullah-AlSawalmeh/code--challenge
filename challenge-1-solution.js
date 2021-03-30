cb6b73357d77d8b75c6d36ffde78e09efef4102c
Abdullah-AlSawalmeh <abdullahalsawalmeh@gmail.com>
2021-03-30 13:34:14 +0300

    Finish

--- challenge-1-solution.js
+++ challenge-1-solution.js
@@ -2,10 +2,16 @@
 let test_array = [1, 4, 3, 5, 6];
 
 function square(test_array) {
+    let sqrtArray = [];
     for (let i = 0; i < test_array.length; i++) {
         console.log(Math.sqrt(test_array[i]));
+        // console.log(4 ** (1 / 2)); ////////// I can use this also ^_^
+        sqrtArray.push(Math.sqrt(test_array[i]))
+
     }
+    return sqrtArray;
 }
 
 
-square(test_array);
+console.log(square(test_array));
+
